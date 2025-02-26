e7a513e22d3afa92b1b6af793b5b97b7d6a6da69
sriniQAE <srinisrini@gmail.com>
2025-02-26 22:13:14 +0530

    BrokenLinks

---
+++ src/test/java/org/collage/Student.java
@@ -0,0 +1,15 @@
+package org.collage;
+
+public class Student {
+	public void studentName() {
+		System.out.println("studentName:srinivasan R");
+	}
+	public void studenDepartment() {
+		System.out.println("studentDepartment:CSE");
+	}
+	public void studenId() {
+		System.out.println("studentId:101");
+		
+	}
+
+}
